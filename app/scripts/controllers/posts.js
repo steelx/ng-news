dafd528f243da2bd3eef2cd3dd4143b8b24724b8
steelx <ajinkyab1987@gmail.com>
2014-07-29 10:06:49 +0000

    Added main stylesheets. And basic Posts controler n view.

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -2,6 +2,16 @@
 
 app.controller('PostsCtrl', ['$scope',
 	function($scope){
+        $scope.posts = [];
         $scope.post = {url: 'http://', title: ''};
+        
+        $scope.submitPost = function(){
+            $scope.posts.push($scope.post);
+            $scope.post = {url: 'http://', title: ''};           
+        };
+        
+        $scope.deletePost = function(index){
+            $scope.posts.splice(index, 1);
+        };
     }
 ]);
