4d1a12f7fbdf6e626245b59e19f93ca14c636918
steelx <ajinkyab1987@gmail.com>
2014-08-03 10:49:24 +0000

    Updated posts.js Controller to redirect user to Single post view after submitting content.

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -3,14 +3,15 @@
 * Controller
 * posts.js
 */
-app.controller('PostsCtrl', ['$scope','Post',
-	function($scope, Post){
+app.controller('PostsCtrl', ['$scope','Post', '$location',
+	function($scope, Post, $location){
         $scope.posts = Post.all;
         $scope.post = {url: 'http://', title: ''};
         
         $scope.submitPost = function(){
-            Post.create($scope.post).then(function(){
-           		$scope.post = {url: 'http://', title: ''};  
+            Post.create($scope.post).then(function(ref){
+           		$location.path('/posts/' + ref.name());
+                console.log(ref);
             });
                      
         };
