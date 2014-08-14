8579c0e7d7642360665d1a9e1b49e62158c6d746
Ajinkya Borade <ajinkyab1987@gmail.com>
2014-08-15 01:07:10 +0530

    Updated profile Controller-views to display Users submited posts in profile.

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,16 +5,18 @@
 */
 app.controller('PostsCtrl', ['$scope','Post', '$location', 'Auth',
 	function($scope, Post, $location, Auth){
-        $scope.posts = Post.all;
+        if($location.path('/')){
+					$scope.posts = Post.all;
+				}
         $scope.post = {url: 'http://', title: ''};
-        
+
         $scope.submitPost = function(){
             Post.create($scope.post).then(function(postId){
            		$location.path('/posts/' + postId);
             });
-                     
+
         };
-        
+
         $scope.deletePost = function(postId){
             Post.delete(postId);
         };
--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -7,6 +7,14 @@ app.controller('ProfileCtrl', ['$scope','$routeParams','Post','User',
   function($scope, $routeParams, Post, User){
     $scope.user = User.findByUsername($routeParams.username);
 
-    
+    $scope.user.$on('loaded', populatePosts);
+
+    function populatePosts(){
+      $scope.posts = {};
+
+      angular.forEach($scope.user.posts, function(postId){
+        $scope.posts[postId] = Post.find(postId);
+      });
+    }
   }
 ]);
