019adfa11219a127f26dd25b2c934bf2ad002b9c
Ajinkya Borade <ajinkyab1987@gmail.com>
2014-08-22 18:42:00 +0530

    Added loader for Register and login. fixed some minnor ui.

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -10,30 +10,34 @@ app.controller('AuthCtrl', ['$scope', '$location', 'Auth', 'User',
         if(Auth.signedIn()){
             $location.path('/');//redirect to homepage if user already logged in.
         }
-        
+
         //Login check for hard page reload
         $scope.$on('$firebaseSimpleLogin:login', function(){
             $location.path('/');//redirect to homepage if user already logged in.
         });
-        
+
         $scope.login = function(){
+            $scope.progress = true;
             Auth.login($scope.user).then(function(loggedinUser){
                 //After user successful Login
                 console.log(loggedinUser);
                 $location.path('/');
             }, function(error){
+                $scope.progress = false;
                 $scope.error = error.toString();
             });
-            
+
         };
-        
+
         $scope.register = function(){
+            $scope.progress = true;
             Auth.register($scope.user).then(function(authUser){
                 //After user successfuly registered
                 User.create(authUser, $scope.user.username);
                 console.log(authUser);
                 $location.path('/');
             }, function(error){
+                $scope.progress = false;
                 $scope.error = error.toString();
             });
         };
